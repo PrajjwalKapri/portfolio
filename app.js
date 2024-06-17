e5d660e9670f609e6bd3db0ef0863a5f535b7177
prajjwalkapri <prajjwalkapri@gmail.com>
2024-06-17 18:33:27 +0530

    Backend Completed

---
+++ app.js
@@ -0,0 +1,50 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import cors from "cors";
+import { dbConnection } from "./database/connection.js";
+import { errorMiddleware } from "./middlewares/error.js";
+import userRouter from "./routes/userRouter.js";
+import timelineRouter from "./routes/timelineRouter.js";
+import messageRouter from "./routes/messageRouter.js";
+import skillRouter from "./routes/skillRouter.js";
+import softwareApplicationRouter from "./routes/softwareApplicationRouter.js";
+import projectRouter from "./routes/projectRouter.js";
+
+const app = express();
+dotenv.config({ path: "./.env" });
+
+app.use(
+  cors({
+    origin: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/timeline", timelineRouter);
+app.use("/api/v1/message", messageRouter);
+app.use("/api/v1/skill", skillRouter);
+app.use("/api/v1/softwareapplication", softwareApplicationRouter);
+app.use("/api/v1/project", projectRouter);
+
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to My portfolio backend");
+});
+dbConnection();
+app.use(errorMiddleware);
+
+export default app;
