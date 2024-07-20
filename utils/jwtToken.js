5c8db4499add52ff11ddd44033b55e0d39a83549
prajjwalkapri <prajjwalkapri@gmail.com>
2024-07-20 18:24:01 +0530

    Commit

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -7,7 +7,8 @@ export const generateToken = (user, message, statusCode, res) => {
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
       httpOnly: true,
-      sameSite: "none", // Ensure cookies are sent in cross-domain contexts
+      sameSite: "none",
+      secure: true, // Add this line
     })
 
     .json({
